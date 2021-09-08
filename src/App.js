c136f5c3d14de1078ddee1aec5de897b55a0e7b7
ratnappatel <ratnappatel@users.noreply.github.com>
2021-09-08 17:15:03 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import logo from './Logo.jpg';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+        <img src={logo} width="200" height="200" className="App-logo" alt="logo" />
+       <p>
+       <h1>About us</h1>
+        <h2> WHO WE ARE</h2>
+ 
+ Our mission has always focused on helping our customers succeed, wherever they are in their education and professional careers.
+ 
+  
+ 
+ We are bridging the higher education gap by delivering content solutions in new and innovative ways to enrich the learning experience. We are a proven leader in strategic higher education consulting and partnering with educators and institutions globally to achieve success, and our print and digital solutions enable students and instructors worldwide.
+  We encourage learning to be a continuous, lifelong experience – an integral, essential part of every stage of building a career.
+       </p>
     </div>
   );
 }
